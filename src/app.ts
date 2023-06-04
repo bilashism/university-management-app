ef5b117c2f554c9f3747e05ca1fe0516acdce49e
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-06-04 10:54:50 +0600

    initial error handling setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import usersRouter from './app/modules/users/users.route';
 const app: Application = express();
 
@@ -9,13 +9,39 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// handle errors
+class APIError extends Error {
+  statusCode: number;
+  constructor(statusCode: number, message: string | undefined, stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+//Testing
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  // res.send('Working Successfully');
+  // throw new APIError(410, "I'm an error!");
+  next("I'm an 'express' error!");
+});
+// global error handler
+app.use((err, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err,
+    });
+  } else {
+    return res.status(500).json({
+      error: 'Something went wrong!',
+    });
+  }
+});
 // Application routes
 
 app.use('/api/v1/users/', usersRouter);
-
-//Testing
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Working Successfully');
-});
 
 export default app;
