d6111bace3b13b31afb4e54d5fbba9e5cf18e07e
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-06-13 19:12:08 +0600

    added createFaculty zod schema

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { academicFacultyController } from './academicFaculty.controller';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
+import { validateRequest } from '../../middlewares/validateRequest';
 
 export const academicFacultyRouter = express.Router();
 
 // create faculty
-academicFacultyRouter.post('/create', academicFacultyController.createFaculty);
+academicFacultyRouter.post(
+  '/create',
+  validateRequest(AcademicFacultyValidation.createAcademicFacultyZodSchema),
+  academicFacultyController.createFaculty
+);
 
 // get faculty
 academicFacultyRouter.get('/', academicFacultyController.getFaculty);
